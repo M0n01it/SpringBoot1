29ade394ced44871bbd3e97be720499bbf6f935e
Nikolay Golovin <iimonolitii@mail.ru>
2025-02-12 23:23:27 +0300

    remove test

--- src/main/java/ru/netology/conditionall1/ConditionalL1Application.java
+++ src/main/java/ru/netology/conditionall1/ConditionalL1Application.java
@@ -2,19 +2,11 @@ package ru.netology.conditionall1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ConditionalL1Application {
 
     public static void main(String[] args) {
-        // Запускаем Spring Boot приложение и получаем контекст
-        ConfigurableApplicationContext context = SpringApplication.run(ConditionalL1Application.class, args);
-
-        // Получаем бин, реализующий интерфейс SystemProfile
-        SystemProfile systemProfile = context.getBean(SystemProfile.class);
-
-        // Выводим текущий профиль в консоль
-        System.out.println(systemProfile.getProfile());
+        SpringApplication.run(ConditionalL1Application.class, args);
     }
 }
