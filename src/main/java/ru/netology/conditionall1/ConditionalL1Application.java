51a1519652de719219a1f8c1115888ecd59fcd2d
Nikolay Golovin <iimonolitii@mail.ru>
2025-02-24 00:47:05 +0300

    add tests

--- src/main/java/ru/netology/conditionall1/ConditionalL1Application.java
+++ src/main/java/ru/netology/conditionall1/ConditionalL1Application.java
@@ -2,9 +2,16 @@ package ru.netology.conditionall1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConditionalL1Application {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConditionalL1Application.class, args);
--- src/test/java/ru/netology/conditionall1/ConditionalL1ApplicationTests.java
+++ src/test/java/ru/netology/conditionall1/ConditionalL1ApplicationTests.java
@@ -1,13 +1,42 @@
 package ru.netology.conditionall1;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ConditionalL1ApplicationTests {
+    @Autowired
+    RestTemplate restTemplate;
+
+    private static final GenericContainer<?> devContainer = new GenericContainer<>("devapp:latest").withExposedPorts(8080);
+    private static final GenericContainer<?> prodContainer = new GenericContainer<>("prodapp:latest").withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        devContainer.start();
+        prodContainer.start();
+    }
 
     @Test
-    void contextLoads() {
+    void devProfileTest() {
+        String devBaseUrl = "http://localhost:" + devContainer.getMappedPort(8080) + "/profile";
+        ResponseEntity<String> response = restTemplate.getForEntity(devBaseUrl, String.class);
+        assertEquals("Current profile is dev", response.getBody());
     }
+
+    @Test
+    void prodProfileTest() {
+        String prodBaseUrl = "http://localhost:" + prodContainer.getMappedPort(8081) + "/profile";
+        ResponseEntity<String> response = restTemplate.getForEntity(prodBaseUrl, String.class);
+        assertEquals("Current profile is production", response.getBody());
+    }
+
 
 }
